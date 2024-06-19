097eeba07f248cf9b4c789d7e4a8dde8a6df6ede
RobertoRobrodri <karmmero@gmail.com>
2024-06-19 19:50:12 +0200

    Script para instalar docker y gsutil

---
+++ start-up.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# instalar docker
+echo UPDATING
+apt-get update && apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# # Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+# apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Instalar gsutil
+apt-get install -y apt-transport-https gnupg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt-get update && apt-get install -y google-cloud-cli
+
+# Bajarse las cosas del bucket
+# source ~/.bashrc
+# gsutil cp -r gs://bucket/folder .
+# Docker compose up
+# cd srcs && make
