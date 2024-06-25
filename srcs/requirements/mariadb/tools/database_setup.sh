2435bfca46716ccc63c06ab74a84246eebc3f602
jalvarad-1 <josealvaradocevallos@gmail.com>
2024-06-25 19:20:24 +0200

    defaults-file mysqld flag correction

--- srcs/requirements/mariadb/tools/database_setup.sh
+++ srcs/requirements/mariadb/tools/database_setup.sh
@@ -18,7 +18,7 @@ GRANT ALL ON ${DATABASE_NAME}.* TO '${MARIADB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-chmod 777 /scripts/init.sql
+#chmod 777 /scripts/init.sql
 # Execute MariaDB
 # https://mariadb.com/kb/en/mysqld_safe/
-mysqld_safe --defaults-file="/scripts/init.sql" --user=mysql --datadir="/var/lib/mysql" --init-file="/scripts/init.sql"
+mysqld_safe --defaults-file="/etc/mysql/my.cnf" --user=mysql --datadir="/var/lib/mysql" --init-file="/scripts/init.sql"
